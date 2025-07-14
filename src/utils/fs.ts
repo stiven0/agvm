d6f1aeef8716f1c4629634e52bba77f04fe191bb
stiven0 <stiven2750@gmail.com>
2025-07-14 17:20:38 -0500

    refactor: run fix prettier

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,4 +1,4 @@
-import { Mode, PathLike, promises as fsp } from 'node:fs';
+import { Mode, PathLike, promises as fsp, WriteFileOptions } from 'node:fs';
 import { FileHandle } from 'node:fs/promises';
 
 import { DataWriteFile } from '../types/fs';
@@ -8,7 +8,8 @@ export const access = (path: string) => fsp.access(path);
 export const readFileUTF8 = (path: string) => fsp.readFile(path, 'utf8');
 export const mkdir = (path: string, options?: { recursive?: boolean }) => fsp.mkdir(path, options);
 export const rm = (path: string, options?: { recursive?: boolean; force?: boolean }) => fsp.rm(path, options);
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const writeFile = (path: PathLike | FileHandle, data: DataWriteFile, options: any) => fsp.writeFile(path, data, options);
 export const chmod = (path: PathLike, mode: Mode) => fsp.chmod(path, mode);
 export const rename = (oldPath: PathLike, newPath: PathLike) => fsp.rename(oldPath, newPath);
+export const writeFile = (path: PathLike | FileHandle, data: DataWriteFile, options: WriteFileOptions) => {
+  return fsp.writeFile(path, data, options);
+};
